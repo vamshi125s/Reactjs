f3b11d22398f3a738ca75169808ac05ffdbc1582
vamshi125s <vamshi14908@gmail.com>
2025-02-03 12:53:38 +0530

    Pushing react concepts

---
+++ src/App.js
@@ -0,0 +1,23 @@
+// import React from "react";
+// function App() {
+//   let ele = React.createElement("div",{className:"App"},
+//     React.createElement("h1",null,"Header")
+//   )
+//   let Username="Vamshi";
+//   return (
+//     // <div className="App">
+//     //   <h1>Header</h1>
+//     // </div>
+//     // ele
+//     // <section>
+//     //   <h1>One jsx element</h1>
+//     //   <p>dvew</p>
+//     // </section>
+//     <div>
+//       <h1>{Username}</h1>
+//     </div>
+
+//   );
+// }
+
+// export default App;
