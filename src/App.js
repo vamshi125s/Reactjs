322564034d2141114c68c942b6f76f9bf936d14e
vamshi125s <vamshi14908@gmail.com>
2025-02-04 12:59:59 +0530

    Pushing react components

--- src/App.js
+++ src/App.js
@@ -21,3 +21,43 @@
 // }
 
 // export default App;
+
+// **** component composition
+//rafcc
+// import React from 'react'
+// import Footer from './components/Footer'
+// import Main from './components/Main'
+// import Navbar from './components/Navbar'
+// import Sidebar1 from './components/Sidebar1'
+// import Sidebar2 from './components/Sidebar2'
+// import './global.css'
+
+// const App = () => {
+//   return (
+//     <div lclassName="app">
+//         <Navbar/>
+//         <Main/>
+//         <div className='Sidebar'>
+//             <Sidebar1/>
+//             <Sidebar2/>
+//         </div>
+//         <Footer/>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// props
+//**********
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
+}
---
+++ src/components/Sidebar2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sidebar2 = () => {
+  return (
+    <div>Sidebar2</div>
+  )
+}
+
+export default Sidebar2
